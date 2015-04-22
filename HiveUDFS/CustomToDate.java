6a9c39defcad940d429c544c11ce3b6af5136549
MattSchool <Matt@172-16-201-213.DYNAPOOL.NYU.EDU>
2015-04-22 16:36:39 -0400

    created folder for Hive UDFs

---
+++ HiveUDFS/CustomToDate.java
@@ -0,0 +1,17 @@
+package com.BigDataProject.hive;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+
+
+//note: to build need to have have hive-exec*.jar in build path
+class CustomToDate extends UDF {
+  public IntWritable evaluate(Text input) {
+  	int year = 2000 + Integer.parseInt(input.substring(5, 6));
+  	int month =  Integer.parseInt(intput.substring(2,3));
+  	int day = Integer.parseInt(input.substring(0,1));
+    return new IntWritable(year*10000+month*100+day);
+  }
+}
